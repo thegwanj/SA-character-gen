d376759f489c684775040a916a5b1f6ff72a3828
Gwanju <gwanjuchung@yahoo.com>
2022-12-11 12:57:31 -0800

    Updating header to link back to homepage

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <p>header test</p>
-        </div>
-    );
+            <header>
+                <nav>
+                    <Link to="/">
+                        <h1>SA Character Sheet Maker</h1>
+                    </Link>
+                </nav>
+            </header>
+        );
 }
 
 export default Header;
