e8ca00e3542bb04ed9452facb85959980ee90e06
Gwanju <gwanjuchung@yahoo.com>
2022-12-10 21:56:04 -0800

    Adding in imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 // Import dependencies
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import pages
+import CreateCharacter from './pages/CreateCharacter';
 
 // Import components
-
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
 
