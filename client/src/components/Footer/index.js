507575a05b168eee8f185a1ef485617c17b785f1
Gwanju <gwanjuchung@yahoo.com>
2022-12-11 19:00:38 -0800

    Updating footer to be a footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-            <p>footer test</p>
-        </div>
+        <footer>
+            <p>Footer test</p>
+        </footer>
     );
 }
 
