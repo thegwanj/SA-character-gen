3935eadf00bc83894b1b0c8da79766a72f7bb981
Gwanju <gwanjuchung@yahoo.com>
2022-12-24 01:40:52 -0800

    Renaming fields and adding in treeLevel

--- server/models/Power.js
+++ server/models/Power.js
@@ -2,14 +2,19 @@ const { Schema, model, Types } = require('mongoose');
 
 // Schema for a power tree, not individual power
 const powerSchema = new Schema({
-    powerId: {
+    treeId: {
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId(),
     },
-    powerName: {
+    treeName: {
         type: String,
         required: true,
-    }
-})
+    },
+    treeLevel: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+});
 
-module.exports = Power;
+module.exports = powerSchema;
