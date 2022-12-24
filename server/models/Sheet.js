75a5c2364b2efee4c0cba542eb904fbf10ccb224
Gwanju <gwanjuchung@yahoo.com>
2022-12-24 00:16:06 -0800

    Developing the sheet. Added in the top portion of the sheet that does not require lists

--- server/models/Sheet.js
+++ server/models/Sheet.js
@@ -2,6 +2,87 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const sheetSchema = new Schema({
+  // Top of the sheet - stuff that is always required
+  playerName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  characterName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  faction: {
+    type: String,
+    required: true,
+  },
+  subFaction: {
+    type: String,
+    required: true,
+  },
+
+  // Specifics for Shifters
+  rank: {
+    type: Number,
+  },
+  deedName: {
+    type: String,
+    trim: true,
+  },
+
+  // Specifics for Vampires
+  generation: {
+    type: Number,
+  },
+  sire: {
+    type: String,
+    trim: true,
+  },
+
+  // Specifics for Wraith
+  passion: {
+    type: String,
+    trim: true,
+  },
+  shadow: {
+    type: String,
+    trim: true,
+  },
+
+  // Health, WP, Energy, and Virtues
+  health: {
+    type: Number,
+    default: 10,
+    required: true,
+  },
+  willpower: {
+    type: Number,
+    default: 1,
+    required: true,
+  },
+
+  // Leaving no default for energy and virtue since new rules are coming up and different factions have different defaults
+  energy: {
+    type: Number,
+    required: true,
+  },
+  virtue: {
+    type: Number,
+    required: true,
+  },
+
+  // XP + Freebies costs
+  xpCost: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  freebiesCost: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
   // left this in because it could be cool to know when a sheet was added
   createdAt: {
     type: Date,
