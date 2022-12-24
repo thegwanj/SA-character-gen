7bb99550113cf79b0abc80e2284a0adafc6d8b8c
Gwanju <gwanjuchung@yahoo.com>
2022-12-24 02:02:39 -0800

    Testing out typeDefs

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,6 +1,13 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Query {
+        sheets: [Sheet]
+        sheet(sheetID: ID!): Sheet
+    }
+    type Mutation {
+        addSheet(playerName: String!, characterName: String!, faction: String!, subFaction: String!, rank: Number, deedName: String, generation: Number, sire: String, passion: String, shadow: String, health: Number, willpower: Number, energy: Number, virtue: Number, xpCost: Number, freebiesCost: Number, powers: powerSchema, skills: skillSchema, lores: loreSchema, merits: meritSchema, patron: String, notes: String): Sheet
+    }
 `;
 
 module.exports = typeDefs;
