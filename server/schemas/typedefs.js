054fc1d13b27a56a1369c39eb3fce9573b4f3cc5
Gwanju <gwanjuchung@yahoo.com>
2022-12-24 02:06:57 -0800

    Adding in type defs for sheet

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,6 +1,32 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Sheet {
+        _id: ID
+        sheetID: Number
+        playerName: String
+        characterName: String
+        faction: String
+        subFaction: String
+        rank: Number
+        deedName: String
+        generation: Number
+        sire: String
+        passion: String
+        shadow: String
+        health: Number
+        willpower: Number
+        energy: Number
+        virtue: Number
+        xpCost: Number
+        freebiesCost: Number
+        powers: [Power]
+        skills: [Skill]
+        lores: [lore]
+        merits: [merit]
+        patron: String
+        notes: String
+    }
     type Query {
         sheets: [Sheet]
         sheet(sheetID: ID!): Sheet
