4fae54f7d53b45a7f4f38dd17f79d569cfd18f74
Gwanju <gwanjuchung@yahoo.com>
2022-12-27 12:54:27 -0800

    Exporting Power

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const Sheet = require('./Sheet');
 const Lore = require('./Lore');
 const Skill = require('./Skill');
 const Merit = require('./Merit');
+const Power = require('./Power');
 
-module.exports = { Sheet, Lore, Merit, Skill };
+module.exports = { Sheet, Lore, Merit, Power, Skill };
