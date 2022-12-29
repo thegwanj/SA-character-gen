608f6d832f69bc39343b9280a5238f0f04fcf712
Gwanju <gwanjuchung@yahoo.com>
2022-12-29 14:07:10 -0800

    Creating the bare bones form and submission handler

--- client/src/components/CreateCharacterForm/index.js
+++ client/src/components/CreateCharacterForm/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const CreateCharacterForm = () => {
+    // TODO: Create functions to handle the form submission
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        // Template
+        try {
 
-};
+        }
+        catch (e){
+            console.error(e);
+        }
+    };
+
+    return (
+        <div class="characterForm">
+            <form onSubmit={handleFormSubmit}>
+                <button type="submit">Create Character</button>
+            </form>
+        </div>
+    );
+}
 
 export default CreateCharacterForm;
