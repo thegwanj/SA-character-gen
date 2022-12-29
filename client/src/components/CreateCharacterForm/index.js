fd88027236c3d2beb081b1efc590a4610df4c73c
Gwanju <gwanjuchung@yahoo.com>
2022-12-29 14:12:27 -0800

    Adding in templates

--- client/src/components/CreateCharacterForm/index.js
+++ client/src/components/CreateCharacterForm/index.js
@@ -14,9 +14,40 @@ const CreateCharacterForm = () => {
         }
     };
 
+    // Template
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        if (name === 'friendName' && value.length <= 280) {
+          setFriendName(value);
+          // setFriendNote(value);
+          setCharacterCount(value.length);
+        }else  if( name === "friendNote" && value.length <= 280 ){
+           setFriendNote(value);
+    
+        }
+      };    
+
     return (
         <div class="characterForm">
             <form onSubmit={handleFormSubmit}>
+                <input
+                name=""
+                placeholder=""
+                value={friendName}
+                className=""
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+                ></input>
+                <textarea
+                name="="
+                placeholder=""
+                value={friendNote}
+                className=""
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+
                 <button type="submit">Create Character</button>
             </form>
         </div>
