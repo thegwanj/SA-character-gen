4b5fdfd9bdc644cc3bf465282acedbf9f5aea95a
Gwanju <gwanjuchung@yahoo.com>
2022-12-29 11:12:28 -0800

    Adding in the character form

--- client/src/pages/CreateCharacter.js
+++ client/src/pages/CreateCharacter.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import CreateCharacterForm from '../components/CreateCharacterForm';
 
+// TODO: Create and implement a form component
 const CreateCharacter = () => {
     return (
         <main class="createCharacterMain">
-            <div class="characterForm">
-                <p>CreateCharacter test</p>
-
-                <Link to="/ViewSheet">
+            <CreateCharacterForm />
+            <Link to="/ViewSheet">
                     <h3>Testing route to ViewSheet</h3>
-                </Link>
-            </div>
+            </Link>
         </main>
     );
-}
+};
 
 export default CreateCharacter;
