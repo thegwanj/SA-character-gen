1ae124ab399dedf1ad328ebe3ddb39e678eee540
Gwanju <gwanjuchung@yahoo.com>
2023-01-20 11:16:50 -0800

    Creating a todo to help guide development

--- client/src/pages/ViewSheet.js
+++ client/src/pages/ViewSheet.js
@@ -3,6 +3,9 @@ import { useQuery } from "@apollo/client";
 import { QUERY_SINGLE_SHEET } from "../utils/queries";
 
 const ViewSheet = () => {
+    // TODO: Create a function(s) that will query the db and get the sheet that was just made. Then, take the information in the sheet and display it on the page
+
+    
     return (
         <div>
             <p>ViewSheet test</p>
