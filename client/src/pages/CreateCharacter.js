d24d0fc5b19ecd12f02480bbb713915cf215c262
Gwanju <gwanjuchung@yahoo.com>
2023-01-20 15:53:37 -0800

    Creating a test link to ViewSheet for deployment

--- client/src/pages/CreateCharacter.js
+++ client/src/pages/CreateCharacter.js
@@ -6,6 +6,9 @@ const CreateCharacter = () => {
     return (
         <main class="createCharacterMain">
             <CreateCharacterForm />
+            <Link to="/ViewSheet">
+                <div class="homeButton"><h3>ViewSheet Test</h3></div>
+            </Link>
         </main>
     );
 };
