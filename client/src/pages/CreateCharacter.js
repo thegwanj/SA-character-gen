76ad27257f173031311c9a2ba7aae032118e155a
Gwanju <gwanjuchung@yahoo.com>
2023-01-20 11:14:23 -0800

    Removing a todo that was completed

--- client/src/pages/CreateCharacter.js
+++ client/src/pages/CreateCharacter.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import CreateCharacterForm from '../components/CreateCharacterForm';
 
-// TODO: Create and implement a form component
 const CreateCharacter = () => {
     return (
         <main class="createCharacterMain">
