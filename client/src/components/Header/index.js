a8d002a8cf52a982da5e2f583ffd95c3bf519874
Gwanju <gwanjuchung@yahoo.com>
2023-01-20 15:45:49 -0800

    Potential fix to logo not showing up in deployed app

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,13 +7,13 @@ const Header = () => {
                 <nav>
                     <div class="navContainer">
                             <Link to="/SA-character-gen">
-                                <img src="../img/logos/shadow-accord-logo-bw.png" alt="SA logo" height="40"/>
+                                <img src="%PUBLIC_URL%/img/logos/shadow-accord-logo-bw.png" alt="SA logo" height="40"/>
                             </Link>
                             <Link to="/SA-character-gen">
                                 <h2>SA Character Sheet Maker</h2>
                             </Link>
                             <Link to="/SA-character-gen">
-                                <img src="../img/logos/shadow-accord-logo-bw.png" alt="SA logo" height="40"/>
+                                <img src="%PUBLIC_URL%/img/logos/shadow-accord-logo-bw.png" alt="SA logo" height="40"/>
                             </Link>
                     </div>
                 </nav>
