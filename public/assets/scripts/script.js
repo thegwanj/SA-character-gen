7078beb70437f0cd5764be2499aeb1e5382aaac2
Gwanju <gwanjuchung@yahoo.com>
2023-03-09 00:04:00 -0800

    Implemented localStorage as a storage solution as opposed to JSON

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -36,26 +36,34 @@ if(window.location.pathname === '/viewSheet'){
 }
 
 // Function that gets info from db
-const getSheet = () => 
-    fetch('/api/sheet', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+const getSheet = () => {
+    //let sheet = JSON.parse(localStorage.getItem("characterSheet"));
+    characterName.innerHTML = localStorage.getItem("character");
+    playerName.innerHTML = localStorage.getItem("player");
+    faction.innerHTML = localStorage.getItem("faction");
+    subfaction.innerHTML = localStorage.getItem("subfaction");
+    patron.innerHTML = localStorage.getItem("patron");
+    note.innerHTML = localStorage.getItem("note");
 
-const renderSheet = async (sheet) => {
+    //renderSheet(sheet);
+}
+    // fetch('/api/sheet', {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     }
+    // });
+
+
+const renderSheet = (sheet) => {
     console.log("Logging sheet", sheet);
 
-    let jsonSheet = await sheet.json();
-    console.log("Logging jsonSheet", jsonSheet);
-
-    characterName.innerHTML = jsonSheet.character;
-    playerName.innerHTML = jsonSheet.player;
-    faction.innerHTML = jsonSheet.faction;
-    subfaction.innerHTML = jsonSheet.subfaction;
-    patron.innerHTML = jsonSheet.patron;
-    note.innerHTML = jsonSheet.note;
+    characterName.innerHTML = sheet.character;
+    playerName.innerHTML = sheet.player;
+    faction.innerHTML = sheet.faction;
+    subfaction.innerHTML = sheet.subfaction;
+    patron.innerHTML = sheet.patron;
+    note.innerHTML = sheet.note;
 }
 
 // Function that handles going to viewSheet page
@@ -67,7 +75,6 @@ const viewSheet = () =>
 const saveSheet = (sheet) =>
     fetch('/api/sheet', {
         method: 'POST',
-        cache: "force-cache",
         headers: {
         'Content-Type': 'application/json',
         },
@@ -77,15 +84,31 @@ const saveSheet = (sheet) =>
 // Function that creates a sheet and sends it off to get saved in the db
 const createSheet = (e) => {
     e.preventDefault();
-    const newSheet = {
-        player: playerName.value,
-        character: characterName.value,
-        faction: faction.value,
-        subfaction: subfaction.value,
-        patron: patron.value,
-        note: note.value
-    };
-    saveSheet(newSheet).then(viewSheet);
+    // const newSheet = {
+    //     player: playerName.value,
+    //     character: characterName.value,
+    //     faction: faction.value,
+    //     subfaction: subfaction.value,
+    //     patron: patron.value,
+    //     note: note.value
+    // };
+
+    // console.log(newSheet);
+
+    // let newContent = JSON.stringify(newSheet);
+
+    // // TODO: Instead of making a file, save to localstorage
+    // localStorage.setItem("characterSheet", JSON.stringify(newContent));
+
+    localStorage.setItem("player", playerName.value);
+    localStorage.setItem("character", characterName.value);
+    localStorage.setItem("faction", faction.value);
+    localStorage.setItem("subfaction", subfaction.value);
+    localStorage.setItem("patron", patron.value);
+    localStorage.setItem("note", note.value);
+
+    //saveSheet(newSheet).then(viewSheet);
+    viewSheet();
 }
 
 // Adding event listeners
@@ -94,7 +117,7 @@ if(window.location.pathname === '/createSheet'){
 }
 
 // Function that gets sheet from db and renders it to the page
-const getAndRenderSheet = () => getSheet().then(renderSheet);
+const getAndRenderSheet = () => getSheet();
 
 if(window.location.pathname === '/viewSheet'){
     getAndRenderSheet();
