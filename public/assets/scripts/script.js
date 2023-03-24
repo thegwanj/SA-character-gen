3908ad63e90bc04b10e9bbe09d191e1eb515ef53
Gwanju <gwanjuchung@yahoo.com>
2023-03-24 14:18:14 -0700

    Added in handling the sheet render if the user faction is shifter

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -20,6 +20,9 @@ let auspiceLabel;
 let tribeLabel;
 let subfactionLabel;
 
+// Variables for faction specific viewing tables
+let shifterSubfaction;
+
 // Variables for future versions
 let xpCost;
 let freebies;
@@ -64,19 +67,50 @@ if(window.location.pathname === '/viewSheet'){
     subfaction = document.getElementById('subfaction');
     patron = document.getElementById('patron');
     note = document.getElementById('notes');
+
+    shifterSubfaction = document.getElementById('shifterSubfaction');
+    breed = document.getElementById('breed');
+    auspice = document.getElementById('auspice');
+    tribe = document.getElementById('tribe');
 }
 
 // Function that gets info from db
 const getSheet = () => {
     //let sheet = JSON.parse(localStorage.getItem("characterSheet"));
-    characterName.innerHTML = localStorage.getItem("character");
-    playerName.innerHTML = localStorage.getItem("player");
-    faction.innerHTML = localStorage.getItem("faction");
-    subfaction.innerHTML = localStorage.getItem("subfaction");
-    patron.innerHTML = localStorage.getItem("patron");
-    note.innerHTML = localStorage.getItem("note");
+    // characterName.innerHTML = localStorage.getItem("character");
+    // playerName.innerHTML = localStorage.getItem("player");
+    // faction.innerHTML = localStorage.getItem("faction");
+    // subfaction.innerHTML = localStorage.getItem("subfaction");
+    // patron.innerHTML = localStorage.getItem("patron");
+    // note.innerHTML = localStorage.getItem("note");
 
-    //renderSheet(sheet);
+    // Sheet object that will get passed into the renderSheet function
+    let sheet = {};
+
+    // Create the sheet based off of faction
+    if(localStorage.getItem("faction") == "Shifter"){
+        sheet = {
+            player: localStorage.getItem("player"),
+            character: localStorage.getItem("character"),
+            faction: localStorage.getItem("faction"),
+            breed: localStorage.getItem("breed"),
+            auspice: localStorage.getItem("auspice"),
+            tribe: localStorage.getItem("tribe"),
+            patron: localStorage.getItem("patron"),
+            note: localStorage.getItem("note")
+        }
+    } else {
+        sheet = {
+            player: localStorage.getItem("player"),
+            character: localStorage.getItem("character"),
+            faction: localStorage.getItem("faction"),
+            subfaction: localStorage.getItem("subfaction"),
+            patron: localStorage.getItem("patron"),
+            note: localStorage.getItem("note")
+        }
+    }
+
+    renderSheet(sheet);
 }
     // fetch('/api/sheet', {
     //     method: 'GET',
@@ -89,12 +123,26 @@ const getSheet = () => {
 const renderSheet = (sheet) => {
     console.log("Logging sheet", sheet);
 
-    characterName.innerHTML = sheet.character;
-    playerName.innerHTML = sheet.player;
-    faction.innerHTML = sheet.faction;
-    subfaction.innerHTML = sheet.subfaction;
-    patron.innerHTML = sheet.patron;
-    note.innerHTML = sheet.note;
+    // Render based off of faction
+    if(sheet.faction == "Shifter"){
+        shifterSubfaction.hidden = false;
+
+        characterName.innerHTML = sheet.character;
+        playerName.innerHTML = sheet.player;
+        faction.innerHTML = sheet.faction;
+        breed.innerHTML = sheet.breed;
+        auspice.innerHTML = sheet.auspice;
+        tribe.innerHTML = sheet.tribe;
+        patron.innerHTML = sheet.patron;
+        note.innerHTML = sheet.note;
+    } else {
+        characterName.innerHTML = sheet.character;
+        playerName.innerHTML = sheet.player;
+        faction.innerHTML = sheet.faction;
+        subfaction.innerHTML = sheet.subfaction;
+        patron.innerHTML = sheet.patron;
+        note.innerHTML = sheet.note;    
+    }
 }
 
 // Function that handles going to viewSheet page
